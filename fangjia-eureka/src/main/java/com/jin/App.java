2f4172d8593c33650f6709da1da8c1b937027956
yiwuxia <740393778@qq.com>
2019-07-01 16:45:20 +0800

    lijin

---
+++ fangjia-eureka/src/main/java/com/jin/App.java
@@ -0,0 +1,18 @@
+package com.jin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Hello world!
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class App
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class,args);
+    }
+}
